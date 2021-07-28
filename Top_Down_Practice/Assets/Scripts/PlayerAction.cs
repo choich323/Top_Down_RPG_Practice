4792422a7352a4beaaa262c6b9bc2b73c8ab0f5e
Jong Hyun Choi <70011908+choich323@users.noreply.github.com>
2021-07-28 23:59:26 +0900

    Add player move without diagonal line

--- Top_Down_Practice/Assets/Scripts/PlayerAction.cs
+++ Top_Down_Practice/Assets/Scripts/PlayerAction.cs
@@ -7,7 +7,7 @@ public class PlayerAction : MonoBehaviour
     public float speed;
     float h;
     float v;
-
+    bool isHorizonMove;
     Rigidbody2D rigid;
 
     void Awake()
@@ -18,12 +18,27 @@ public class PlayerAction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Move value
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+        // Check button down & up
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        // Check horizontal move
+        if (hDown || vUp)
+            isHorizonMove = true;
+        else if (vDown || hUp)
+            isHorizonMove = false;
     }
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v) * speed;    
+        // Move
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * speed;    
     }
 }

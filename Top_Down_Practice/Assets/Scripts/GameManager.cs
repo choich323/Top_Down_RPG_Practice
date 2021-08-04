67b98332feaef32205027c50d725f2daab7b8272
Jong Hyun Choi <70011908+choich323@users.noreply.github.com>
2021-08-04 17:58:36 +0900

    Set Talk Panel Active / Inactive

--- Top_Down_Practice/Assets/Scripts/GameManager.cs
+++ Top_Down_Practice/Assets/Scripts/GameManager.cs
@@ -5,13 +5,27 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject talkPanel;
     public Text talkText;
     public GameObject scan_object;
+
+    // 현재 액션 실행중인가
+    public bool isAction;
 
     // Update is called once per frame
     public void Action(GameObject scanObj)
     {
-        scan_object = scanObj;
-        talkText.text = scan_object.name + "(이)가 있다.";
+        if (isAction)   // Exit Action
+        {
+            isAction = false;
+        } 
+        else            // Enter Action
+        {
+            isAction = true;
+            scan_object = scanObj;
+            talkText.text = scan_object.name + "(이)가 있다.";
+        }
+
+        talkPanel.SetActive(isAction);  // 액션키를 누르면 대화창을 활성화
     }
 }
--- Top_Down_Practice/Assets/Scripts/PlayerAction.cs
+++ Top_Down_Practice/Assets/Scripts/PlayerAction.cs
@@ -41,14 +41,14 @@ public class PlayerAction : MonoBehaviour
     void Update()
     {
         // Move value
-        h = Input.GetAxisRaw("Horizontal");
-        v = Input.GetAxisRaw("Vertical");
+        h = manager.isAction ? 0 : Input.GetAxisRaw("Horizontal");
+        v = manager.isAction ? 0 : Input.GetAxisRaw("Vertical");
 
         // Check button down & up
-        bool hDown = Input.GetButtonDown("Horizontal");
-        bool vDown = Input.GetButtonDown("Vertical");
-        bool hUp = Input.GetButtonUp("Horizontal");
-        bool vUp = Input.GetButtonUp("Vertical");
+        bool hDown = manager.isAction ? false : Input.GetButtonDown("Horizontal");
+        bool vDown = manager.isAction ? false : Input.GetButtonDown("Vertical");
+        bool hUp = manager.isAction ? false : Input.GetButtonUp("Horizontal");
+        bool vUp = manager.isAction ? false : Input.GetButtonUp("Vertical");
 
         // Check horizontal move
         if (hDown)
@@ -83,7 +83,7 @@ public class PlayerAction : MonoBehaviour
         else if (hDown && h == 1)
             dirVec = Vector3.right;
 
-        //Scan object
+        //Scan Object & Action
         if (Input.GetButtonDown("Jump") && scan_object != null)
             manager.Action(scan_object);
 

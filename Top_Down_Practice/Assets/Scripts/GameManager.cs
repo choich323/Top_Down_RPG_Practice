7f4902005a0d9e0c9c3345c18e8879b87b700964
Jong Hyun Choi <70011908+choich323@users.noreply.github.com>
2021-08-05 17:04:47 +0900

    Add Talk System

--- Top_Down_Practice/Assets/Scripts/GameManager.cs
+++ Top_Down_Practice/Assets/Scripts/GameManager.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public TalkManager talkManager;
     public GameObject talkPanel;
     public Text talkText;
     public GameObject scan_object;
+    public int talkIndex;
 
     // 현재 액션 실행중인가
     public bool isAction;
@@ -15,17 +17,35 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     public void Action(GameObject scanObj)
     {
-        if (isAction)   // Exit Action
-        {
-            isAction = false;
-        } 
-        else            // Enter Action
-        {
-            isAction = true;
-            scan_object = scanObj;
-            talkText.text = scan_object.name + "(이)가 있다.";
-        }
+        isAction = true;
+        scan_object = scanObj;
+        ObjData objData = scan_object.GetComponent<ObjData>();
+        Talk(objData.id, objData.isNPC);
 
         talkPanel.SetActive(isAction);  // 액션키를 누르면 대화창을 활성화
+    }
+
+    void Talk(int id, bool isNPC)
+    {
+        string talkData = talkManager.GetTalk(id, talkIndex);
+        
+        if(talkData == null)
+        {
+            isAction = false;
+            talkIndex = 0;
+            return;
+        }
+
+        if (isNPC)
+        {
+            talkText.text = talkData;
+        }
+        else
+        {
+            talkText.text = talkData;
+        }
+
+        isAction = true;
+        talkIndex++;
     }
 }
---
+++ Top_Down_Practice/Assets/Scripts/TalkManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkManager : MonoBehaviour
+{
+    Dictionary<int, string[]> talkData; // 두 개의 타입을 적어야 함(키, 밸류)
+
+    void Awake()
+    {
+        talkData = new Dictionary<int, string[]>();
+        GenerateData();
+    }
+
+    // Update is called once per frame
+    void GenerateData()
+    {
+        talkData.Add(1000, new string[] { "안녕?", "이 곳은 처음이지?" });
+        talkData.Add(2000, new string[] { "이 뒤로는 갈 필요없어.", "가봤자 아무것도 없거든.", "못 믿겠으면 직접 확인해보던가." });
+        talkData.Add(100, new string[] { "평범한 나무상자다." });
+        talkData.Add(200, new string[] { "Luna가 준비한 책상이다." });
+    }
+
+    public string GetTalk(int id, int talkIndex)
+    {
+        if (talkIndex == talkData[id].Length)
+            return null;
+        else
+            return talkData[id][talkIndex];
+    }
+}
